bb0646bf09cccda1bb9e979d7ee1f30fe25f20fc
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2025-01-14 17:02:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function to determine if the year is a leap year
 function isLeapYear(year) {
-  return (year % 4 == 0) && (year % 100 != 0) && (year % 400 != 0) ? `${year} is a leap year.` : `${year} is not a leap year.`
+  return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)) ? `${year} is a leap year.` : `${year} is not a leap year.`;
 }
 
 // Declare Variables
